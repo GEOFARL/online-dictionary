c37470b740e85e14e22b65479ff28912f7f02e5b
Maxim <toper.one111@gmail.com>
2024-03-29 21:34:48 +0200

    feat: add http request loggs od-75

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -3,4 +3,5 @@ export {
 	requiresAuthMiddleware,
 } from "./authenticate/authenticate.js";
 export { errorHandlerMiddleware } from "./error-handler/error-handler.js";
+export { httpLoggerMiddleware } from "./http-logger/http-logger.js";
 export { validateMiddleware } from "./validate/validate.js";
